6f8b3042152d9f11b25e81ea53391bc0f7895523
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-11 21:37:17 +0530

    Update yell.py

--- etc/yell.py
+++ etc/yell.py
@@ -2,7 +2,7 @@ def main():
     yell("This","is","cs50")
 
 def yell(*words):
-    # uppercased = map(str.upper,words)#using map() function
+    uppercased = map(str.upper,words)#using map() function
     uppercased = [word.upper() for word in words]#using list comprehensions
     print(*uppercased)
 
